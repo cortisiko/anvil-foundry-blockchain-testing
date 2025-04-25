041333024d4a11f14dba136faddf2a5a32678051
Curtis David <Curtis.David7@gmail.com>
2025-04-25 16:36:22 -0400

    add documentation: readme and jsdocs

--- anvil-utils/AnvilClients.ts
+++ anvil-utils/AnvilClients.ts
@@ -1,6 +1,14 @@
 import { createPublicClient, createWalletClient, http } from "viem";
 import { CUSTOM_CHAIN } from "./config";
 
+/**
+ * Creates anvil clients for the custom chain.
+ * Read more about viem here: https://viem.sh/docs/clients/intro
+ * @param port - The port number of the anvil node
+ * @returns The public and wallet clients
+ */
+
+
 function CreateAnvilClients({ port }: { port: number }) {
   const publicClient = createPublicClient({
     chain: CUSTOM_CHAIN,
